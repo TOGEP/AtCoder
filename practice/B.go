ff8d5e3d229669d5a02a1bb6d1a31e1f3b63b399
TOGEP <sg.endo0302@gmail.com>
2022-01-25 21:49:18 +0900

    Commit practice/B point100...

---
+++ practice/B.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+)
+
+func swap(a *string, b *string) {
+	tmp := *a
+	*a = *b
+	*b = tmp
+}
+
+// クイックソート
+// Q = 1000は間に合うという前提 -> Qの値による終了処理は行わない
+// TODO 毎回最小値がヒポットとして選ばれているのでO(n^2)...
+//      N個のラベル付けをランダムに取得して平均O(n log n)にしたい
+// TODO Q = 7の時
+func quickSort(str []string, lenght int) (ret []string) {
+	if lenght < 2 {
+		return str
+	}
+
+	pivot := str[0]
+
+	left := []string{}
+	right := []string{}
+
+	for _, v := range str[1:] {
+		fmt.Printf("? %s %s\n", v, pivot)
+		var ans string
+		fmt.Scanf("%s", &ans)
+		if ans == ">" {
+			right = append(right, v)
+		} else {
+			left = append(left, v)
+		}
+	}
+
+	right = quickSort(right, len(right))
+	left = quickSort(left, len(left))
+
+	ret = append(left, pivot)
+	ret = append(ret, right...)
+
+	return ret
+}
+
+func main() {
+	var N, Q int
+
+	fmt.Scanf("%d %d", &N, &Q)
+
+	str := make([]string, N)
+
+	for i, s := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		if i == N {
+			break
+		}
+		str[i] = string(s)
+	}
+
+	ret := quickSort(str, N)
+
+	fmt.Printf("! ")
+	for _, v := range ret {
+		fmt.Printf("%s", v)
+	}
+	fmt.Printf("\n")
+}
